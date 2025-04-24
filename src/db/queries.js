430da4ccd5227fae47e1c903a9b0ca24a3c790dc
Julian So <ch.julian.so@gmail.com>
2025-04-23 21:01:31 -0400

    add query to link games to genres

--- src/db/queries.js
+++ src/db/queries.js
@@ -8,27 +8,16 @@ async function getAllGames() {
 
 async function addNewGame(name, genres) {
   // await pool.query('INSERT INTO video_games (video_game_name) VALUES ($1)', [name]);
-  // const genreTest = await pool.query(`INSERT INTO genre (genre_name) SELECT UNNEST(?::TEXT[])`, [
-  // const videoGame = await pool.query(`SELECT video_game_id FROM video_games
-  //                   WHERE video_game_name = ${name}`);
-  // const genreTest = await pool.query(`INSERT INTO genre (genre_name) SELECT UNNEST($1::TEXT[])`, [
-  //   genres,
-  // ]);
+
   const genreV2 = await pool.query(
     `
-    INSERT INTO game_genre (video_game_id, genre_id)
-    VALUES
+      (
+      SELECT video_game_id, genre_id
+            FROM (SELECT video_game_id
+                  FROM video_games WHERE video_game_name = 'Starcraft') AS vgact
+                  CROSS JOIN (SELECT genre_id FROM genre WHERE genre_name = any(ARRAY['strategy','Action']::TEXT[])) AS ggact
+      )  
 
-    UNNEST(
-      (SELECT video_game_id
-      FROM video_games
-      WHERE video_game_name = $2),
-      
-      SELECT genre_id
-      FROM genre
-      WHERE genre_name IN
-      (SELECT * FROM UNNEST($1::TEXT[])
-    )
     `,
     [genres, name]
   );
